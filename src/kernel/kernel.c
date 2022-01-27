7e8857674dfead35b5fff92a5d53cddcbb997118
EnumInt <zoomer589@gmail.com>
2022-01-27 16:02:28 -0500

    IDT base

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -117,8 +117,16 @@ static int get_model(void)
 }
 
 
-
-
+// LDT
+struct InterruptDescriptor64 {
+   uint16_t offset_1;        // offset bits 0..15
+   uint16_t selector;        // a code segment selector in GDT or LDT
+   uint8_t  ist;             // bits 0..2 holds Interrupt Stack Table offset, rest of bits zero.
+   uint8_t  type_attributes; // gate type, dpl, and p fields
+   uint16_t offset_2;        // offset bits 16..31
+   uint32_t offset_3;        // offset bits 32..63
+   uint32_t zero;            // reserved
+};
 
 // The following will be our kernel's entry point.
 void _start(struct stivale2_struct *stivale2_struct) {
