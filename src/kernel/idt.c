29bfb85e03a67cb94f26eb0a708f09988dbf5821
EnumInt <zoomer589@gmail.com>
2022-01-28 18:33:03 -0500

    add almost all IDT code

--- src/kernel/idt.c
+++ src/kernel/idt.c
@@ -1,11 +1,25 @@
 #include <idt.h>
+#include <stddef.h>
 #include <stdint.h>
 
-int idt_init() {
-   // Load the IDT
-   uint16_t selector = 0x28;
-   uint64_t myHandlerAddress = (uint64_t);
-   idt.offset_1 = myHandlerAddress & 0xFFFF;
-   idt.offset_2 = (myHandlerAddress >> 16) & 0xFFFF;
-   idt.offset_3 = myHandlerOffset >> 32;
+__attribute__((noreturn))
+void exception_handler(void);
+void exception_handler() {
+
+  __asm__ volatile("cli; hlt"); // Completely hangs the computer
 }
+
+
+void idt_set_descriptor(uint8_t vector, void* isr, uint8_t flags);
+void idt_set_descriptor(uint8_t vector, void* isr, uint8_t flags) {
+    struct InterruptDescriptor64* descriptor = &idt[vector];
+ 
+    descriptor->isr_low        = (uint64_t)isr & 0xFFFF;
+    descriptor->kernel_cs      = 0x28;
+    descriptor->ist            = 0;
+    descriptor->attributes     = flags;
+    descriptor->isr_mid       = ((uint64_t)isr >> 16) & 0xFFFF;
+    descriptor->isr_high      = ((uint64_t)isr >> 32) & 0xFFFFFFFF;
+    descriptor->reserved           = 0;
+}
+
--- src/kernel/idt.h
+++ src/kernel/idt.h
@@ -1,19 +1,27 @@
-#ifndef IDT_H
-#define IDT_H
+#pragma once
 
 #include <stddef.h>
+
 #include <stdint.h>
 
 struct InterruptDescriptor64 {
-   uint16_t offset_1;        // offset bits 0..15
-   uint16_t selector;        // a code segment selector in GDT or LDT
-   uint8_t  ist;             // bits 0..2 holds Interrupt Stack Table offset, rest of bits zero.
-   uint8_t  type_attributes; // gate type, dpl, and p fields
-   uint16_t offset_2;        // offset bits 16..31
-   uint32_t offset_3;        // offset bits 32..63
-   uint32_t zero;            // reserved
-}__attribute__((packed));
+  uint16_t isr_low; // The lower 16 bits of the ISR's address
+  uint16_t kernel_cs; // The GDT segment selector that the CPU will load into CS before calling the ISR
+  uint8_t ist; // The IST in the TSS that the CPU will load into RSP; set to zero for now
+  uint8_t attributes; // Type and attributes; see the IDT page
+  uint16_t isr_mid; // The higher 16 bits of the lower 32 bits of the ISR's address
+  uint32_t isr_high; // The higher 32 bits of the ISR's address
+  uint32_t reserved; // Set to zero
+}
+__attribute__((packed)) InterruptDescriptor64;
 
 struct InterruptDescriptor64 idt[256];
- 
-#endif
+
+// DO I NEED THIS??? static idtr_t idtr;
+
+struct idtr_t {
+  uint16_t limit;
+  uint32_t base;
+}
+__attribute__((packed)) idtr_t;
+
