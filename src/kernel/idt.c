794daa477bbd800e79f0fa2a2866a79cf32608a9
EnumInt <zoomer589@gmail.com>
2022-01-28 15:44:42 -0500

    IDT work

--- src/kernel/idt.c
+++ src/kernel/idt.c
@@ -3,10 +3,9 @@
 
 int idt_init() {
    // Load the IDT
-
    uint16_t selector = 0x28;
-   uint64_t myHandlerAddress = (uint64_t)MyHandler;
-   idtEntry.offset_1 = myHandlerAddress & 0xFFFF;
-   idtEntry.offset_2 = (myHandlerAddress >> 16) & 0xFFFF;
-   idtEntry.offset_3 = myHandlerOffset >> 32;
+   uint64_t myHandlerAddress = (uint64_t);
+   idt.offset_1 = myHandlerAddress & 0xFFFF;
+   idt.offset_2 = (myHandlerAddress >> 16) & 0xFFFF;
+   idt.offset_3 = myHandlerOffset >> 32;
 }
