5e4ccd5897055e441737883bec74c52aae196d20
EnumInt <zoomer589@gmail.com>
2022-01-28 10:42:17 -0500

    FIX THE LITTLE BUGGER

--- src/kernel/idt.c
+++ src/kernel/idt.c
@@ -4,7 +4,7 @@
 int idt_init() {
    // Load the IDT
 
-   uint16_t selector = 0x08;
+   uint16_t selector = 0x28;
    uint64_t myHandlerAddress = (uint64_t)MyHandler;
    idtEntry.offset_1 = myHandlerAddress & 0xFFFF;
    idtEntry.offset_2 = (myHandlerAddress >> 16) & 0xFFFF;
